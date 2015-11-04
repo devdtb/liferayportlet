6d1965f1b50ef9823307cca9896136317e094164
devdtb <devdtb@gmail.com>
2015-11-04 10:34:42 +0100

    Mere

--- liferayportlet/src/main/java/dtb/test/MyPortletUI.java
+++ liferayportlet/src/main/java/dtb/test/MyPortletUI.java
@@ -30,7 +30,7 @@ public class MyPortletUI extends UI {
 			+ "Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. "
 			+ "Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. "
 			+ "Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. "
-			+ "Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad "
+			+ "Vestibulum lacinia arcu egetum nulla. Class aptent taciti sociosqu ad "
 			+ "litora torquent per conubia nostra, per inceptos himenaeos.</p>"
 			+ "<p><a href=\"https://github.com/devdtb/vapo\" target=\"_blank\">GitHub</a></p>"
 			+ "<hr>", ContentMode.HTML);
